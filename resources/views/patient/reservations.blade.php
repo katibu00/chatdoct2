c6913e4a31598eb44df7ed6f45e90aeeff7141e0
katibu01 <ukmisau@gmail.com>
2024-01-21 17:20:55 +0100

    “update”

--- resources/views/patient/reservations.blade.php
+++ resources/views/patient/reservations.blade.php
@@ -325,44 +325,49 @@
                     <div class="modal fade" id="link{{ $key }}" tabindex="-1" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered mw-650px">
                             <div class="modal-content">
-                                <div class="modal-header" id="kt_modal_new_address_header">
-                                    <h2>Video Chat Link from Dr. {{ $doctor['book']['first_name'] }}
-                                        {{ $doctor['book']['middle_name'] }} {{ $doctor['book']['last_name'] }}</h2>
-                                    <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
-                                        <span class="svg-icon svg-icon-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none">
-                                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2"
-                                                    rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
-                                                <rect x="7.41422" y="6" width="16" height="2" rx="1"
-                                                    transform="rotate(45 7.41422 6)" fill="black" />
-                                            </svg>
-                                        </span>
-                                    </div>
-                                </div>
-                                <div class="modal-body py-10 px-lg-17">
-                                    <div class="scroll-y me-n7 pe-7" id="kt_modal_new_address_scroll"
-                                        data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}"
-                                        data-kt-scroll-max-height="auto"
-                                        data-kt-scroll-dependencies="#kt_modal_new_address_header"
-                                        data-kt-scroll-wrappers="#kt_modal_new_address_scroll"
-                                        data-kt-scroll-offset="300px">
-
-                                        <div class="row mb-5">
-                                            <div class="col-md-12 fv-row">
-                                                <label class="required fs-5 fw-bold mb-2">Link</label>
-                                                <textarea name="link" class="form-control form-control-lg form-control-solid" readonly>{{ $doctor->link == '' ? 'Link Not yet Sent.' : $doctor->link }}</textarea>
-                                            </div>
+                                @if ($doctor['book'])
+                                    <div class="modal-header" id="kt_modal_new_address_header">
+                                        <h2>Video Chat Link from Dr.
+                                            {{ $doctor['book']['first_name'] ?? '' }}
+                                            {{ $doctor['book']['middle_name'] ?? '' }}
+                                            {{ $doctor['book']['last_name'] ?? '' }}
+                                        </h2>
+                                        <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                                            <span class="svg-icon svg-icon-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                                    fill="none">
+                                                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                                        transform="rotate(-45 6 17.3137)" fill="black" />
+                                                    <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                                        transform="rotate(45 7.41422 6)" fill="black" />
+                                                </svg>
+                                            </span>
                                         </div>
-
                                     </div>
-                                </div>
-                                <div class="modal-footer flex-center">
-                                    <button type="reset" data-bs-dismiss="modal" class="btn btn-light me-3">OK</button>
-                                </div>
+                                    <div class="modal-body py-10 px-lg-17">
+                                        <div class="scroll-y me-n7 pe-7" id="kt_modal_new_address_scroll" data-kt-scroll="true"
+                                            data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto"
+                                            data-kt-scroll-dependencies="#kt_modal_new_address_header"
+                                            data-kt-scroll-wrappers="#kt_modal_new_address_scroll" data-kt-scroll-offset="300px">
+                    
+                                            <div class="row mb-5">
+                                                <div class="col-md-12 fv-row">
+                                                    <label class="required fs-5 fw-bold mb-2">Link</label>
+                                                    <textarea name="link"
+                                                        class="form-control form-control-lg form-control-solid" readonly>{{ $doctor->link == '' ? 'Link Not yet Sent.' : $doctor->link }}</textarea>
+                                                </div>
+                                            </div>
+                    
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer flex-center">
+                                        <button type="reset" data-bs-dismiss="modal" class="btn btn-light me-3">OK</button>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                     </div>
+                    
                 @empty
                     <div class="alert alert-warning" role="alert">You have no recent bookings. When you, they will show
                         up here.</div>
